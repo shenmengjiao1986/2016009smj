71e15abd137faf2a1df241d46bdd6a39f2346493
joyceshen19860703 <mengjiao.shen@dianping.com>
2017-08-18 10:56:38 +0800

    new project

---
+++ HiFootball2017/HiFootball2017/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HiFootball2017
+//
+//  Created by joyceshen on 17/8/18.
+//  Copyright © 2017年 joyceshen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
